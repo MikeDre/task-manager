a241ed1f68cac93ca40f7e2dea58a5b6baacacdf
MikeDre <info@mikedre.com>
2024-01-27 08:53:20 +0000

    ✨ Added: base task input form

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,12 @@ import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
+    <h1>Task manager</h1>
+    <form id="tm__form">
+      <input type="text" id="tm__new-task-title" placeholder="Add task name..." required >
+      <button type="submit">Add Task</button>
+    </form>
+
+    <ul id="tm__tasks"></ul>
   </div>
 `
